763503a7e1a6db1d3aa105dd71f3c71905059565
Jatin Verma <jatinsose473@gmail.com>
2026-07-09 16:33:37 +0530

    creating components of the app

---
+++ FootBull/src/components/chatPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const chatPanel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default chatPanel
---
+++ FootBull/src/components/scorePanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const scorePanel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default scorePanel
