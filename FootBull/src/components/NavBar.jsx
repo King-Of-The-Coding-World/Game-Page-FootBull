a1cb442e7def0db8855c82b39a407f010638b680
Jatin Verma <jatinsose473@gmail.com>
2026-07-09 20:36:14 +0530

    creating css files for components

--- FootBull/src/components/NavBar.jsx
+++ FootBull/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
     <div>
-        
+
     </div>
   )
 }
--- FootBull/src/components/chatPanel.jsx
+++ FootBull/src/components/chatPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./chatPanel.css"
 
 const chatPanel = () => {
   return (
