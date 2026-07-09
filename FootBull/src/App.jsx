92f9a42f8e25f8042edaf58e7be55a4bdb98f5af
Jatin Verma <jatinsose473@gmail.com>
2026-07-09 21:54:43 +0530

    fixing bg image error

--- FootBull/src/App.jsx
+++ FootBull/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import scorePanel from "./components/scorePanel";
 import chatPanel from "./components/chatPanel";
+import "./App.css"
 
 function App() {
   const [team1, setTeam1] = useState();
--- FootBull/src/components/NavBar.jsx
+++ FootBull/src/components/NavBar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import "./NavBar.css"
+import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div>
-
+      <nav>
+        <ul>
+          <li>FootBull</li>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Trending</li>
+            <li>Merch</li>
+          </ul>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
